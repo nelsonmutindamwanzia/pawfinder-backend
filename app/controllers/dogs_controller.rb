6979e3c2fab114d507b3579afb9c2abd70be711f
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-12-05 08:12:34 +0300

    add index and show methods to dogs controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,14 @@
 class DogsController < ApplicationController
+    def index
+        render json: Dog.all
+    end
+
+    def show
+        dog = Dog.find(params[:id])
+        render json: dog
+    end
+
+    def create
+
+    end
 end
