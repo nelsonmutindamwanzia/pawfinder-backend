d96de455d959aa6d0b8fedb88acf3417d11581ed
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-12-05 09:25:09 +0300

    add create, update and destroy methods

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -4,11 +4,38 @@ class DogsController < ApplicationController
     end
 
     def show
-        dog = Dog.find(params[:id])
+        dog = find_dog
         render json: dog
     end
 
     def create
+        dog = Dog.create!(dog_params) 
+        render json: dog, status: :created
+    end
 
+    def update 
+        dog = find_dog
+        dog.update(dog_params)
+        render json: dog
+    end
+
+    def destroy
+        dog = find_dog
+        if dog 
+            dog.destroy
+            head :no_content
+        else
+            render json:{ error:"Dog not found" }, status: :not_found
+        end
+    end
+
+    private
+
+    def find_dog
+        Dog.find(params[:id])
+    end
+
+    def dog_params
+        params.permit(:name, :breed, :image_url, :last_seen, :user_id)
     end
 end
