448d0f068ffc00e1be509af31a5dd89f593e3243
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-12-09 15:08:21 +0300

    add /me to allowed origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,4 +13,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
+
+    allow do
+      origins "https://paw-finder-production.up.railway.app/me"
+  
+      resource "*",
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
   end
