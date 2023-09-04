c5aaed85a07ee8ce134406bc8a9c584d46d3a57d
nelsonmutindamwanzia <nmutinda02@gmail.com>
2023-09-04 20:45:15 +0300

    update application.rb file

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,8 @@ require "action_cable/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+# Bundler.require(*Rails.groups)
+Bundler.require(:default, :assets, Rails.env)
 
 module PawFinder
   class Application < Rails::Application
