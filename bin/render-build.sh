e37381de38bb85eda2f321de8e0d1f4f2a48c132
nelsonmutindamwanzia <nmutinda02@gmail.com>
2023-09-04 21:30:31 +0300

    update render-build bash file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/env bash
-# exit on error
 set -o errexit
+
+# the current version of Nokogiri ships with incompatible libs
+bundle config --local build.nokogiri --use-system-libraries
 
 bundle install
 bundle exec rake assets:precompile
